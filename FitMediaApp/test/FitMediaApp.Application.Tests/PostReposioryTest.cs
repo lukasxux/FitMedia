1ee82fd07350db6d3f54ce637333661e3e9eaa66
Lukas Hainzl <hai20266@spengergasse.at>
2023-10-18 23:02:22 +0200

    Unit tests

--- FitMediaApp/test/FitMediaApp.Application.Tests/PostReposioryTest.cs
+++ FitMediaApp/test/FitMediaApp.Application.Tests/PostReposioryTest.cs
@@ -46,34 +46,7 @@ namespace FitMediaApp.Application.Tests
             Assert.True(db.Posts.Count() == 0);
         }
 
-        /*[Fact]
-        public async void DeleteFailIfTaskIsExpiredTest()
-        {
-            // ARRANGE
-            var db = GetDatabase();
-            var repo = new HandinRepository(db);
-
-            var handin = new Handin(
-                student: new Student(studentNr: 1, firstname: "firstname", lastname: "lastname", email: "email"),
-                task: new Task("subject", "title",
-                    team: new Team("name", "schoolclass"),
-                    teacher: new Teacher("firstname", "lastname", "email"),
-                    expirationDate: new DateTime(2021, 1, 1),
-                    maxPoints: null),
-                created: new DateTime(2024, 1, 1),
-                description: "description",
-                documentUrl: "documentUrl");
-            db.Handins.Add(handin);
-            db.SaveChanges();
-            db.ChangeTracker.Clear();
-
-            // ACT
-            var (success, message) = await repo.Delete(handin.Guid);
-
-            // ASSERT
-            Assert.False(success);
-            Assert.True(db.Handins.Count() == 1);
-        }*/
+       
     }
 }
 
