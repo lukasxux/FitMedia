a160eb6551c9483303af3c68c7b669d233dff4db
Lukas Hainzl <hai20266@spengergasse.at>
2024-03-01 14:14:10 +0100

    Profile Picture upload

---
+++ FitMediaApp/src/FitMediaApp.Client/src/views/UploadView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="file" ref="fileInput" accept=".png, .jpg" />
+    <button @click="uploadImage">Upload</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    uploadImage() {
+      console.log("Uploading image...");
+    },
+  },
+};
+</script>
