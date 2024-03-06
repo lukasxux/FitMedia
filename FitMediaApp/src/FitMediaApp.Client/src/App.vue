75e4f99c555592d9792e5aeb65e3a733818c4bdd
Raffael Rabl <rab20362@spengergasse.at>
2024-03-06 09:58:20 +0100

    Sync

--- FitMediaApp/src/FitMediaApp.Client/src/App.vue
+++ FitMediaApp/src/FitMediaApp.Client/src/App.vue
@@ -8,11 +8,17 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="FitMedia logo" class="logo" src="@/assets/Fit-Media_logo.png" width="300" height="100" />
 
     <div class="wrapper">
-      <HelloWorld msg="You dont have an account yet!" />
+      <!-- <HelloWorld msg="You dont have an account yet!" /> -->
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
+
+
+  <div>
+    <input type="file" @change="handleFileUpload">
+    <button @click="saveImageLocally">Bild lokal speichern</button>
+  </div>
     </div>
   </header>
 
@@ -20,3 +26,39 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 <style scoped>
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      imageFile: null
+    };
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.imageFile = event.target.files[0];
+    },
+    saveImageLocally() {
+      if (this.imageFile) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const imageDataUrl = reader.result;
+          this.saveToFileSystem(imageDataUrl);
+        };
+        reader.readAsDataURL(this.imageFile);
+      } else {
+        console.error('Es wurde kein Bild ausgewählt');
+      }
+    },
+    saveToFileSystem(dataUrl) {
+      const link = document.createElement('a');
+      link.href = dataUrl;
+      link.download = 'meinBild.jpg';
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+};
+</script>
